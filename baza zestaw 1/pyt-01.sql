8879a953c5ffb43ddd7721427efd96dd977eb1d7
Damian Gawin <gawin.damian@gmail.com>
2018-04-08 15:15:02 +0200

    update

---
+++ baza zestaw 1/pyt-01.sql
@@ -0,0 +1,15 @@
+SET client_encoding='utf-8';
+-- wyświetl zawartość każdej z tabel, uporządkuj w/g atrybutu
+SELECT * FROM klient
+;
+SELECT * FROM klient ORDER BY nazwisko
+;
+SELECT * FROM towar
+;
+SELECT * FROM towar ORDER BY cena DESC
+;
+
+-- zadanie P1: wyświetl zawartość powyższych tabel uporządkowanych
+-- odpowiednio według miast, opisów;
+-- wyświetl zawartość tabeli zamówień uporządkowanej według kosztów
+-- i tabeli kodów kreskowych uporządkowanej według kodów.
